1e4735cc70003b2fece4d887694b45049a415199
Reginaldo Fernandes <reginaldo.fernands@gmail.com>
2021-01-20 14:17:11 -0300

    Project create

---
+++ backend/src/test/java/com/devifce/difdeliver/DifdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devifce.difdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DifdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
